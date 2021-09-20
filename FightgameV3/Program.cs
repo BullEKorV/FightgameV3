815a7c2380b046d43abf3ed9a0c25acff237afee
Elias <elias.wasielewski@elev.ga.ntig.se>
2021-09-20 09:47:33 +0200

    init comit

---
+++ FightgameV3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FightgameV3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter p1 = new Fighter();
+            Console.ReadLine();
+        }
+    }
+}
